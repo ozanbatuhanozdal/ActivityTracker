bc1fbdbd851a25b2722108c0d9318ce9bc1f1a6e
Ozan Batuhan özdal <ozanbatuhanozdal@hotmail.com>
2021-02-13 14:58:55 +0300

    client side structure and functional component refactor

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,35 @@
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
+import { Button, Header, Icon, List } from 'semantic-ui-react';
+import { IActivity } from '../models/activity';
+
+
+const App = () => {
+
+  const [activities,setActivities] = useState<IActivity[]>([])
+  
+  useEffect(() => {
+    axios.get<IActivity[]>('https://localhost:5001/api/activities')
+    .then(response => {
+      setActivities(response.data);
+    });
+  },[]);
+
+    return(
+      
+      <div>
+      <Header as='h2'> 
+        <Icon name='users'></Icon>
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {activities.map((activity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+      </div>
+    )
+  
+}
+
+export default App;
